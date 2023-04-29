7939f356db8e1bb120918e5c4ff5cc55a87b7e0c
Paulina Szamraj <paulinaps.93@wp.pl>
2023-04-29 15:50:19 +0200

    added currencies with rates

---
+++ src/currencies.js
@@ -0,0 +1,37 @@
+export const currencies = [
+  {
+    name: "Dolar amerykański",
+    short: "Dolar",
+    rate: 4.2855,
+  },
+
+  {
+    name: "Euro",
+    short: "Euro",
+    rate: 4.6785,
+  },
+
+  {
+    name: "Frank szwajcarski",
+    short: "Frank",
+    rate: 4.6989,
+  },
+
+  {
+    name: "Funt brytyjski",
+    short: "Funt",
+    rate: 5.3462,
+  },
+
+  {
+    name: "Korona czeska",
+    short: "Korona",
+    rate: 0.1996,
+  },
+
+  {
+    name: "Yuan chiński",
+    short: "Yuan",
+    rate: 5.7885,
+  },
+];
