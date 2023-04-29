ba268f4227fa82b52218d84d5787706a6a9110d2
Paulina Szamraj <paulinaps.93@wp.pl>
2023-04-29 15:51:15 +0200

    added result interaction with style

---
+++ src/Form/Result/index.js
@@ -0,0 +1,17 @@
+import "./style.css";
+
+const Result = ({ result }) => (
+  <p className="result">
+    Kwota po przeliczeniu:
+    {result !== undefined && (
+      <>
+        <strong>
+          {result.targetAmount.toFixed(2)}
+          {result.currency}
+        </strong>
+      </>
+    )}
+  </p>
+);
+
+export default Result;
